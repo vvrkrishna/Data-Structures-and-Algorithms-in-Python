bf474e515c0f6a386351a05ce482e934bffc6175
vvrkrishna <55209802+vvrkrishna@users.noreply.github.com>
2020-02-05 18:29:41 -0500

    Add files via upload

---
+++ Determining if brackets are balanced using Python.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+"""
+Stack Data Structure.
+"""
+class Stack():
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)				
+
+    def pop(self):
+        return self.items.pop()
+    
+    def is_empty(self):
+        return self.items == []
+    
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+        
+    def get_stack(self):
+        return self.items
+
+
+# In[6]:
+
+
+def is_match(p1, p2):
+    if p1 == "(" and p2 == ")":
+        return True
+    elif p1 == "{" and p2 == "}":
+        return True
+    elif p1 == "[" and p2 == "]":
+        return True
+    else:
+        return False
+
+
+def is_paren_balanced(paren_string):
+    s = Stack()
+    is_balanced = True
+    index = 0
+
+    while index < len(paren_string) and is_balanced:
+        paren = paren_string[index]
+        if paren in "([{":
+            s.push(paren)
+        else:
+            if s.is_empty():
+                is_balanced = False
+            else:
+                top = s.pop()
+                if not is_match(top, paren):
+                    is_balanced = False
+        index += 1
+
+    if s.is_empty() and is_balanced:
+        return True
+    else:
+        return False
+
+
+# In[10]:
+
+
+print(is_paren_balanced("(((({}))))"))
+
+
+# In[8]:
+
+
+print(is_paren_balanced("[][]]]"))
+
+
+# In[9]:
+
+
+print(is_paren_balanced("[][]"))
+
+
+# In[ ]:
+
+
+
+
